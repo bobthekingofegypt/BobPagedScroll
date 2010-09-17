c2971dbab87b7558f42cedacf8aed1f3fba7f9ed
bob <bob@bob.com>
2010-09-17 18:37:49 +0100

    Added in GHUnit and test build target

---
+++ Test/TestUnitTest.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+#import <GHUnitIOS/GHUnitIOS.h>
+
+@interface TestUnitTest : GHTestCase {
+
+}
+
+@end
---
+++ Test/TestUnitTest.m
@@ -0,0 +1,12 @@
+#import "TestUnitTest.h"
+
+
+@implementation TestUnitTest
+
+- (void)testFoo {
+    // Assert a is not NULL, with no custom error description
+    GHAssertNotNULL(@"", nil);
+	
+}
+
+@end
